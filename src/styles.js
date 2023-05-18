3d473797c71b78681383873accad1aa5c5f0b4c6
icchigoo <icchigoo12@gmail.com>
2023-05-18 23:27:06 +0545

    hha

--- src/styles.js
+++ src/styles.js
@@ -1,7 +1,7 @@
 const styles = {
-  paddingX: "sm:px-16 px-6",
+  paddingX: "sm:px-6 px-4", // Adjust the horizontal padding for mobile screens
   paddingY: "sm:py-16 py-6",
-  padding: "sm:px-16 px-6 sm:py-16 py-10",
+  padding: "sm:px-6 px-4 sm:py-16 py-10", // Adjust the horizontal padding for mobile screens
 
   heroHeadText:
     "font-black text-white lg:text-[80px] sm:text-[60px] xs:text-[50px] text-[40px] lg:leading-[98px] mt-2",
